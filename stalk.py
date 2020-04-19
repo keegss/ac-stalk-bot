686b2fd37aaf92358d8b7368268a0062f4897615
Keegan Conway <keegsconway@gmail.com>
2020-04-19 14:07:01 -0400

    make prettier

--- stalk.py
+++ stalk.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
+
 import discord
 import secrets
 
 class StalkBot(discord.Client):
     async def on_message(self, message):
+        # check if bot posted
+        if message.author == self.user: return 
         print('Message from {0.author}: {0.content}'.format(message))
 
 if __name__ == "__main__":
