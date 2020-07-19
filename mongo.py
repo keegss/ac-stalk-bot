a5ba366516781d807ec0fbb032e998b461552484
Keegan Conway <keegsconway@gmail.com>
2020-07-19 01:03:45 -0400

    add user price entry functionality

--- mongo.py
+++ mongo.py
@@ -4,9 +4,26 @@ import pprint
 class Mongo:
     def __init__(self):
         self.client = pymongo.MongoClient("mongodb://localhost:27017/")
-        self.mydb = self.client["villagers"]
+        self.db = self.client["villager-database"]
+        self.villagers = self.db['villagers']
 
-    def enter_user_price(self, user, price, am_or_pm, day):
+    def enter_user_price(self, user: str, price: int, am_or_pm: str, day: str):
+
+        # if user is not present, create and add to db
+        user_entry = self.villagers.find_one({'user': user})
+        if user_entry is None:
+            user_entry = {
+                'user': user,
+                '0': [0, 0],
+                '1': [0, 0],
+                '2': [0, 0],
+                '3': [0, 0],
+                '4': [0, 0],
+                '5': [0, 0]
+            }
+            self.villagers.insert_one(user_entry)
+
+        return self.update_user_data(user_entry, price, am_or_pm, day)
 
         # TODO:
             # when to clear the week??
@@ -15,13 +32,23 @@ class Mongo:
                     # where day is user input
                     # expected day is last day entered
 
-        # if no user
-            # create all 0 for day and enter given day
-        # if user
-            # retrive user data from db and update day price
+    def update_user_data(self, user_entry, price, am_or_pm, day) -> bool:
+        if int(day) < 0 or int(day) > 6:
+            return False
+        
+        if am_or_pm == 'am':
+            user_entry[day][0] = price
+        else:
+            user_entry[day][1] = price
 
-    def predict()
+        query = {'user': user_entry['user']}
+        new_val = {'$set': {day: user_entry[day]}}        
+        self.villagers.update_one(query, new_val)
 
+        return True
+
+    def predict(self):
+        pass
         # is user
             # retrieve user from db
             # format string
@@ -29,33 +56,54 @@ class Mongo:
         # else
             # User has no data associated!
 
-    def close():
+    def close(self):
         self.client.close()
 
 def main():
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
-    db = client['mydatabase']
-    collection = db['test-collection']
-    print('um')
-    user1 = {
-        'user': 'keegs#7270',
-        '0': [0, 0],
-        '1': [0, 0],
-        '2': [0, 0],
-        '3': [0, 0],
-        '4': [0, 0],
-        '5': [0, 0]
-        }
+    mongo = Mongo()
+    mongo.enter_user_price('keegs#7270', 165, 'am', '1')
+    villagers = mongo.db.villagers
+    pprint.pprint(villagers.find_one({'user': 'keegs#7270'}))
+    mongo.close()
 
-    print('test')
-    posts = db.posts
-    print('yo')
-    post_id = posts.insert_one(user1).inserted_id
-    print(post_id)
+    # client = pymongo.MongoClient('mongodb://localhost:27017/')
+    # db = client['mydatabase']
+    # collection = db['test-collection']
+    # print('um')
+    # user1 = {
+    #     'user': 'keegs#7270',
+    #     '0': [0, 0],
+    #     '1': [0, 0],
+    #     '2': [0, 0],
+    #     '3': [0, 0],
+    #     '4': [0, 0],
+    #     '5': [0, 0]
+    #     }
+    # user1 = {
+    #     'user': 'potato#7271',
+    #     '0': [0, 0],
+    #     '1': [0, 0],
+    #     '2': [0, 0],
+    #     '3': [0, 0],
+    #     '4': [0, 0],
+    #     '5': [0, 0]
+    #     }
 
-    pprint.pprint(posts.find_one({'user': 'keegs#7270'}))
+    # print('test')
+    # posts = db.posts
+    # print('yo')
+    # post_id = posts.insert_one(user1).inserted_id
+    # print(post_id)
 
-    client.close()
+    # pprint.pprint(posts.find_one({'user': 'keegs#7270'}))
+
+    # query = {'user': 'keegs#7270'}
+    # new_value = {'$set': {'1': [160, 0]}} 
+    # posts.update_one(query, new_value)
+
+    # pprint.pprint(posts.find_one({'user': 'keegs#7270'}))
+
+    # client.close()
 
 if __name__ == '__main__':
     main()
