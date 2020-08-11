e002e5fbcb6c22abaa0eb3295564245be47fb992
Keegan Conway <keegsconway@gmail.com>
2020-08-10 20:56:07 -0400

    clean up user entry and update code

--- mongo.py
+++ mongo.py
@@ -8,7 +8,8 @@ class Mongo:
         self.db = self.client["villager-database"]
         self.villagers = self.db['villagers']
 
-    def enter_user_price(self, user: str, price: int, am_or_pm: str, day: str):
+    def enter_user_price(self, user: str, price: int, am_or_pm: str, day=None):
+        if day == None: day = date.today().weekday()
 
         # if user is not present, create and add to db
         user_entry = self.villagers.find_one({'user': user})
@@ -23,36 +24,37 @@ class Mongo:
                 '4': [0, 0],
                 '5': [0, 0]
             }
+            user_entry[str(day)][0 if am_or_pm == 'am' else 1] = price
             self.villagers.insert_one(user_entry)
+        else:
+            return self.update_user_data(user_entry, price, am_or_pm, day)
 
-        return self.update_user_data(user_entry, price, am_or_pm, day)
+    def reset_user(self, user: str):
+        reset_user_entry = {
+            'user': user_entry['user'],
+            'expected_day': data.today.weekday(),
+            '0': [0, 0],
+            '1': [0, 0],
+            '2': [0, 0],
+            '3': [0, 0],
+            '4': [0, 0],
+            '5': [0, 0]
+        }
+        self.villagers.delete_one({'user': user_entry['user']})
+        self.villagers.insert_one(reset_user_entry)
 
     def update_user_data(self, user_entry, price, am_or_pm, day) -> bool:
-        if int(day) < 0 or int(day) > 6:
+        if day < 0 or day > 6:
             return False
 
-        if int(day) < user_entry['expected_day']:
-            # reset users week data
-            reset_user_entry = {
-                'user': user_entry['user'],
-                'expected_day': user_entry['expected_day'],
-                '0': [0, 0],
-                '1': [0, 0],
-                '2': [0, 0],
-                '3': [0, 0],
-                '4': [0, 0],
-                '5': [0, 0]
-            }
-            self.villagers.delete_one({'user': 'keegs#7270'})
-            self.villagers.insert_one(reset_user_entry)
+        # TODO: reassess expected day reset logic          
 
-        if am_or_pm == 'am':
-            user_entry[day][0] = price
-        else:
-            user_entry[day][1] = price
+        # mongo docs must have strings as keys
+        str_day = str(day)
+        user_entry[str_day][0 if am_or_pm == 'am' else 1] = price
 
         query = {'user': user_entry['user']}
-        new_val = {'$set': {day: user_entry[day]}}        
+        new_val = {'$set': {str_day: user_entry[str_day]}}        
         self.villagers.update_one(query, new_val)
 
         return True
@@ -71,50 +73,11 @@ class Mongo:
 
 def main():
     mongo = Mongo()
-    mongo.enter_user_price('keegs#7270', 165, 'am', '1')
     villagers = mongo.db.villagers
-    pprint.pprint(villagers.find_one({'user': 'keegs#7270'}))
+    villagers.drop()
+    mongo.enter_user_price('keegs#7270', 185, 'am')
     pprint.pprint(villagers.find_one({'user': 'keegs#7270'}))
     mongo.close()
-
-    # client = pymongo.MongoClient('mongodb://localhost:27017/')
-    # db = client['mydatabase']
-    # collection = db['test-collection']
-    # print('um')
-    # user1 = {
-    #     'user': 'keegs#7270',
-    #     '0': [0, 0],
-    #     '1': [0, 0],
-    #     '2': [0, 0],
-    #     '3': [0, 0],
-    #     '4': [0, 0],
-    #     '5': [0, 0]
-    #     }
-    # user1 = {
-    #     'user': 'potato#7271',
-    #     '0': [0, 0],
-    #     '1': [0, 0],
-    #     '2': [0, 0],
-    #     '3': [0, 0],
-    #     '4': [0, 0],
-    #     '5': [0, 0]
-    #     }
-
-    # print('test')
-    # posts = db.posts
-    # print('yo')
-    # post_id = posts.insert_one(user1).inserted_id
-    # print(post_id)
-
-    # pprint.pprint(posts.find_one({'user': 'keegs#7270'}))
-
-    # query = {'user': 'keegs#7270'}
-    # new_value = {'$set': {'1': [160, 0]}} 
-    # posts.update_one(query, new_value)
-
-    # pprint.pprint(posts.find_one({'user': 'keegs#7270'}))
-
-    # client.close()
 
 if __name__ == '__main__':
     main()
